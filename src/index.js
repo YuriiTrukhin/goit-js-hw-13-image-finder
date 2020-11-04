e6bb0ca1d062389a51b0b4a2386a8f053cd6544a
YuriiTrukhin <68285301+YuriiTrukhin@users.noreply.github.com>
2020-11-04 17:11:52 +0200

    light box

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import template from "./template/templateList.hbs"
 import {alert} from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
+import * as basicLightbox from 'basiclightbox'
+import "../node_modules/basiclightbox/dist/basicLightbox.min.css";
 
-// let btn = document.querySelector(".button")
-let input = document.querySelector('[type="text"]');
+
+let input = document.querySelector('input');
 let ul = document.querySelector(".gallery")
 let page = 1;
 let a = 100;
 
 const addCard = function () {
-    console.log(input.value);
     api(input.value,page)
         .then((data) => {
             if (data.hits.length >= 1) {
                 data.hits.forEach(el => {            
-                    ul.insertAdjacentHTML("beforeend", template(el))                  
-                // btn.classList.remove("is-hidden")
+                    ul.insertAdjacentHTML("beforeend", template(el))           
         })
         } else {
             alert("Таких картин не найдено, введите другой запрос");
@@ -29,9 +29,8 @@ const addCard = function () {
     ul.innerHTML = "";   
 }
 
-const loadMore = function () {    
-           
-         api(input.value, page += 1)
+const loadMore = function () {          
+    api(input.value, page += 1)
     .then((data) => {
         data.hits.forEach(el => { 
             document.documentElement > a + 100 
@@ -50,7 +49,17 @@ const scroll = function () {
         setTimeout(fn,75)
     }
 }
-// btn.addEventListener("click",loadMore)
-input.addEventListener("change", addCard )
+
+const lightBox = function (e) {
+    if (e.target.nodeName === "IMG") {
+        const instance = basicLightbox.create(`
+    <img src=${e.target.dataset["name"]}>
+`)
+        instance.show()
+    }else {""} 
+}
+
+ul.addEventListener("click",lightBox)
+input.addEventListener("change",addCard)
 window.addEventListener('scroll',scroll);
 
